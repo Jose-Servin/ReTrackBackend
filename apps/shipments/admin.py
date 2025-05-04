f266ec276467649d54eb44c0bfbcf5836a149a24
Jose Servin <servinjose96@gmail.com>
2025-05-04 09:20:43 -0500

    Set up Shipment app list page

--- apps/shipments/admin.py
+++ apps/shipments/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Carrier)
+class CarrierAdmin(admin.ModelAdmin):
+    list_display = ["name", "mc_number", "created_at", "updated_at"]
+
+
+@admin.register(models.CarrierContact)
+class CarrierContactAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "carrier", "role", "is_primary"]
+    list_editable = ["is_primary"]
+    list_select_related = ["carrier"]
+
+
+@admin.register(models.Shipment)
+class ShipmentAdmin(admin.ModelAdmin):
+    list_display = [
+        "origin",
+        "destination",
+        "current_status",
+        "carrier",
+        "driver",
+        "vehicle",
+    ]
+    list_select_related = ["carrier", "driver", "vehicle"]
+
+
+@admin.register(models.ShipmentStatusEvent)
+class ShipmentStatusEventAdmin(admin.ModelAdmin):
+    list_display = ["shipment", "status", "event_timestamp", "source"]
+
+
+@admin.register(models.Driver)
+class DriverAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "phone_number", "email", "carrier"]
+
+
+@admin.register(models.Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ["carrier", "plate_number", "device_id"]
--- retrack/urls.py
+++ retrack/urls.py
@@ -19,6 +19,8 @@ from django.contrib import admin
 from django.urls import path, include
 import debug_toolbar
 
+admin.site.site_header = "ReTrackBackend"
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("__debug__/", include(debug_toolbar.urls)),
